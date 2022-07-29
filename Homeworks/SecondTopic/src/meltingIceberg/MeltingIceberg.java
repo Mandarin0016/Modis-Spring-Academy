b3e575a1aeafcc5b22385e44fdbe8f22efdbd358
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-07-29 22:10:50 +0300

    Modis-Java-Academy

--- Homeworks/SecondTopic/src/meltingIceberg/MeltingIceberg.java
+++ Homeworks/SecondTopic/src/meltingIceberg/MeltingIceberg.java
@@ -1,86 +1,71 @@
 package meltingIceberg;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class MeltingIceberg {
     private static long iceCubesCount;
     private static int hours;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = Integer.parseInt(scanner.nextLine());
-
-        char[][] matrix = initiateMatrix(scanner, n);
-
-        char[][] tempMatrix = new char[n][n];
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new FileInputStream("C:\\Modis_Java_Academy\\Homeworks\\SecondTopic\\src\\meltingIceberg\\data.txt"));
+        char[][] matrix = initiateMatrix(scanner);
 
         while (iceCubesCount > 0) {
-            iceCubesCount = updateIceCubes(matrix, iceCubesCount, tempMatrix);
+            updateCubesPerOneHour(matrix);
             hours++;
-            updateMyMatrix(matrix, tempMatrix);
         }
+
         System.out.println(hours);
     }
 
-    private static char[][] initiateMatrix(Scanner scanner, int n) {
-        char[][] matrix = new char[n][n];
+    private static void updateCubesPerOneHour(char[][] matrix) {
+        replaceMeltingCubes(matrix);
+        updateMyMatrix(matrix);
+    }
 
-        for (int row = 0; row < n; row++) {
+    private static void replaceMeltingCubes(char[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[row][col] == '*' && isMelting(matrix, row, col)) {
+                    matrix[row][col] = '@';
+                }
+            }
+        }
+    }
+
+    private static void updateMyMatrix(char[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix.length; col++) {
+                if (matrix[row][col] == '@') {
+                    matrix[row][col] = '0';
+                    iceCubesCount--;
+                }
+            }
+        }
+    }
+
+    private static char[][] initiateMatrix(Scanner scanner) {
+        int matrixSize = Integer.parseInt(scanner.nextLine());
+        char[][] matrix = new char[matrixSize][matrixSize];
+        for (int row = 0; row < matrixSize; row++) {
             String line = scanner.nextLine();
             matrix[row] = line.toCharArray();
             char iceCube = '*';
             iceCubesCount += line.chars().filter(ch -> ch == iceCube).count();
         }
-
         return matrix;
-    }
-
-    private static long updateIceCubes(char[][] matrix, long iceCubesCount, char[][] tempMatrix) {
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix.length; col++) {
-                char currentCell = matrix[row][col];
-                if (currentCell == '0') {
-                    tempMatrix[row][col] = '0';
-                } else if (currentCell == '*') {
-                    if (isMelting(matrix, row, col)) {
-                        tempMatrix[row][col] = '0';
-                        iceCubesCount--;
-
-                    } else {
-                        tempMatrix[row][col] = '*';
-                    }
-                }
-            }
-        }
-        return iceCubesCount;
-    }
-
-    private static void updateMyMatrix(char[][] matrix, char[][] tempMatrix) {
-        for (int row = 0; row < tempMatrix.length; row++) {
-            for (int col = 0; col < tempMatrix[row].length; col++) {
-                matrix[row][col] = tempMatrix[row][col];
-            }
-        }
-
     }
 
     private static boolean isMelting(char[][] matrix, int row, int col) {
         int countZeros = 0;
-        if (matrix[row + 1][col] == '0') {
+        if (matrix[row + 1][col] == '0' || matrix[row - 1][col] == '0') {
             countZeros++;
         }
-        if (matrix[row - 1][col] == '0') {
+        if (matrix[row][col + 1] == '0' || matrix[row][col - 1] == '0') {
             countZeros++;
         }
-        if (matrix[row][col + 1] == '0') {
-            countZeros++;
-        }
-        if (matrix[row][col - 1] == '0') {
-            countZeros++;
-        }
-        if (countZeros >= 2) {
-            return true;
-        }
-        return false;
+        return countZeros >= 2;
     }
 }
