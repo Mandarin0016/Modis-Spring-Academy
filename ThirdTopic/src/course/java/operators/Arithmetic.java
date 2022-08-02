88544e5fa6778f60658aa0fb34400659c6a401a2
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-08-02 23:36:24 +0300

    Delete Arithmetic.java

--- ThirdTopic/src/course/java/operators/Arithmetic.java
+++
@@ -1,8 +0,0 @@
-package course.java.operators;
-
-public class Arithmetic {
-    public static void main(String[] args) {
-        var result = 1L/0.5F;
-        System.out.println(1L/0.5F);
-    }
-}
