981978ab32eea24fa85d6483a6a08fea16cf2397
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-08-18 10:53:03 +0300

    Delete FourthTopic directory

--- FourthTopic/src/main/java/course/java/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package course.java.dao;
-
-import course.java.model.User;
-
-import java.util.Optional;
-
-public interface UserRepository extends Repository<User, Long>{
-    Optional<User> findByUsername(String username);
-}
