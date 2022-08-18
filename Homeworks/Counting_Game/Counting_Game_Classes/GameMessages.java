50df42d18d85d103eb618b9fed67495f1a50f7d5
Viktor Aleksandrov <100943442+Mandarin0016@users.noreply.github.com>
2022-08-18 10:54:43 +0300

    Modis-Java-Academy

---
+++ Homeworks/Counting_Game/Counting_Game_Classes/GameMessages.java
@@ -0,0 +1,3 @@
+public final class GameMessages {
+    public final static String GAME_WINNER = "The winner of the current Counting Game is player with number -> %d!";
+}
---
+++ Homeworks/Counting_Game/Counting_Game_Classes/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO: In order to run the game, just right click -> run and write two numbers separated by space
+        //create new game Engine
+        GameEngine engine = new GameEngine();
+        //run the game
+        engine.run();
+    }
+
+}
